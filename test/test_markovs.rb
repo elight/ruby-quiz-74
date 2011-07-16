950bfc6c049c1471f61071af973bc0454aab4fad
Evan Light <evan@tripledogdare.net>
2011-07-15 23:13:08 -0400

    Two passing tests but still failing on doing actual chaining who'd a thunk it

--- test/test_markovs.rb
+++ test/test_markovs.rb
@@ -1,7 +1,34 @@
 require 'helper'
 
-class TestMarkovs < MiniTest::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+class MarkovGenerator
+  def train_with(source_data)
+  end
+
+  def generate_string_for(seed_word)
+    ""
+  end
+end
+
+
+
+class TestMarkovs < MiniTest::Spec
+  describe "Simplest possible Markov Chain example" do
+    before do
+      source_data = "fuck you"
+      @generator = MarkovGenerator.new
+      @generator.train_with source_data
+    end
+
+    it "should output 'fuck you' when given input 'fuck'" do
+      @generator.generate_string_for('fuck').must_equal 'fuck you'
+    end
+
+    it "should output '' when given input 'you'" do
+      @generator.generate_string_for('you').must_equal ''
+    end
+
+    it "should output '' when given input 'ohai'" do
+      @generator.generate_string_for('ohai').must_equal ''
+    end
   end
 end
