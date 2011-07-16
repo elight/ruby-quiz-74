ad96b9e21f597a6712e1451e0588919a623ba9d7
Evan Light <evan@tripledogdare.net>
2011-07-15 23:35:02 -0400

    herp derp markov chain test cases pass

--- test/test_markovs.rb
+++ test/test_markovs.rb
@@ -1,11 +1,30 @@
 require 'helper'
 
 class MarkovGenerator
+  def initialize
+    @knowledge = {}
+  end
+
   def train_with(source_data)
+    tokens = source_data.split
+    tokens.each_with_index do |word, index|
+      if index < tokens.length
+        @knowledge[word] = tokens[index+1]
+      end
+    end
   end
 
   def generate_string_for(seed_word)
-    ""
+    next_word = choose_word_to_follow(seed_word)
+    if next_word
+      "#{seed_word} #{choose_word_to_follow(seed_word)}"
+    else
+      ""
+    end
+  end
+
+  def choose_word_to_follow(seed_word)
+    @knowledge[seed_word]
   end
 end
 
@@ -14,21 +33,56 @@ end
 class TestMarkovs < MiniTest::Spec
   describe "Simplest possible Markov Chain example" do
     before do
-      source_data = "fuck you"
       @generator = MarkovGenerator.new
-      @generator.train_with source_data
     end
 
-    it "should output 'fuck you' when given input 'fuck'" do
-      @generator.generate_string_for('fuck').must_equal 'fuck you'
+    describe "with seed data 'fuck you'" do
+      before do
+        @generator.train_with 'fuck you'
+      end
+
+      it "should output 'fuck you' when given input 'fuck'" do
+        @generator.generate_string_for('fuck').must_equal 'fuck you'
+      end
+
+      it "should output '' when given input 'you'" do
+        @generator.generate_string_for('you').must_equal ''
+      end
+
+      it "should output '' when given input 'ohai'" do
+        @generator.generate_string_for('ohai').must_equal ''
+      end
     end
 
-    it "should output '' when given input 'you'" do
-      @generator.generate_string_for('you').must_equal ''
-    end
+    describe "with seed data 'can haz'" do
+      before do
+        @generator.train_with 'can haz'
+      end
 
-    it "should output '' when given input 'ohai'" do
-      @generator.generate_string_for('ohai').must_equal ''
+      it "should output 'can haz' when given input 'can'" do
+        @generator.generate_string_for('can').must_equal 'can haz'
+      end
+
+      it "should output '' when given input 'haz'" do
+        @generator.generate_string_for('haz').must_equal ''
+      end
+
+      it "should output '' when given input 'ohai'" do
+        @generator.generate_string_for('ohai').must_equal ''
+      end
+    end
+  end
+
+  describe "Training the generator" do
+    describe "given input 'can haz'" do
+      before do
+        @generator = MarkovGenerator.new
+        @generator.train_with('can haz')
+      end
+
+      it "should map can -> haz'" do
+        @generator.choose_word_to_follow('can').must_equal 'haz'
+      end
     end
   end
 end
